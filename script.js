bdeee091565e9e583ca3696c1b42a893284e9c63
SWAPnil <105651714+swapnilithub@users.noreply.github.com>
2024-08-08 10:49:42 +0530

    Add files via upload
    finat commit

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log(a) // initialization first
+
+var a = 7; //decleration after Initialization
+
+{
+console.log(b)
+var b = "Swapnil";
+}
+
+//the above shown is the example of hoisting in javascript its works only with var and not with 
+// let and const 
+
+// ** lets take the example of const and let
+{
+//it will give the error -> console.log(d) 
+// because init before dec-> let d = 45;
+}
+//same goes for the let
+git config --global user.name "Swapnil Ranjan"
+git config --global user.email "mailtoswapnilr@gmail.com"
+
+git add .
+  git commit -m "Initial commit"
+git remote add origin https://github.com/swapnilithub/replit-hoisting_js.git
+git push -u origin master
